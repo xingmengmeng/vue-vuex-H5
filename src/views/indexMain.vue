e3fca5c0e6c1e02f005f44a9b4140b3e179e942c
xingmengmeng <xingmengmeng@gomeholdings.com>
2018-01-17 18:31:46 +0800

    修改配置

--- src/views/indexMain.vue
+++ src/views/indexMain.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        登录页面+5  时间很快
+        登录页面+5  时间很快s
     </div>
 </template>
 <script>
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,5 +1,5 @@
 <template>
     <section>
-        列表页面刷新测试
+        列表页面刷新测试ss
     </section>
 </template>
